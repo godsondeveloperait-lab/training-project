37a6c6762caa90beded5eb1605ff211e9b90496e
jagangait <jaganganesanait@gmail.com>
2026-03-07 12:28:10 +0530

    seed with multiple command

---
+++ src/seeds/seed.products.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../app.module';
+import { ProductsService } from 'src/products/products.service';
+
+async function bootstrap() {
+  const app = await NestFactory.createApplicationContext(AppModule);
+
+  const productsService = app.get(ProductsService);
+
+  await productsService.createProduct({
+    name: 'Asus Laptop',
+    description: 'laptop product',
+    price: 100,
+    stock: 10,
+    images: ['image1.jpg', 'image2.jpg']
+  });
+
+  console.log('Seed completed');
+
+  await app.close();
+}
+
+bootstrap();
